97f8ef6268f6101d8c06fc7d06d585e394002349
Rajesh kumar <119530020+Rajesh2112@users.noreply.github.com>
2025-06-28 04:28:03 +0000

    datatypes

---
+++ javascript/02_datatypes.js
@@ -0,0 +1,21 @@
+"use scrict" // all the javascript code is consider as a new version code
+
+// alert(3+3) // simple alert is not used in node.
+
+let name="Rajesh Chaudhary"
+let age=23
+let isloggedIn= true
+
+// number =its range is 2^53
+// string ""
+// bigInt
+// boolean  true or false
+// symbol unique 
+// undefined the value is not assigned
+
+
+// object
+console.log(typeof "Rajesh Chaudhary")
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof  undefined) //undefined
